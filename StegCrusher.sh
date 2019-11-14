6318557299661c1de85e5f1c9cf6c91becb1120c
mamatb <amatbaeza@gmail.com>
2019-11-14 14:39:03 +0000

    TODOs added, unnecessary trailing ";" removed, quotes replaced

--- StegCrusher.sh
+++ StegCrusher.sh
@@ -10,88 +10,89 @@
 
 # TODO
 #
-# readme.md with screenshots and license
+# readme.md
 # replace file extension check with magic numbers check
-# save dictionary fragments in the /tmp directory
+# save dictionary fragments in /tmp/
 # show progress while cracking
 # use colored output
 # replace GNU Parallel with built-in parallel mechanisms
+# parse arguments with getops
 
 # number of threads = number of available processing units
 THREADS=$(nproc)
 
 # other declarations
-WORDLIST_FRAGMENT_NAME=".StegCrusher_tmp_"
+WORDLIST_FRAGMENT_NAME='.StegCrusher_tmp_'
 print_usage() {
-	echo -e "Usage:\n\t${0} <stego_file> <wordlist_file>\n\toutput file will be <stego_file>.out in case of success"
+	echo -e 'Usage:\n\t'"${0}"' <stego_file> <wordlist_file>\n\toutput file will be <stego_file>.out in case of success'
 }
 
 # arguments usage check
-if [ "${#}" != "2" ];
+if [ "${#}" != '2' ]
 then
 	print_usage
 	exit 1
 fi
 
 # parallel installation check (parallel computing tool)
-if [ ! "$(which parallel)" ];
+if [ ! "$(which parallel)" ]
 then
-	echo "ERROR - you need to have parallel installed in order to use the script \"${0}\""
-	echo "INFO - installation in Debian-based distros: sudo apt install parallel"
+	echo 'ERROR - you need to have parallel installed in order to use the script "'"${0}"'"'
+	echo 'INFO - installation in Debian-based distros: sudo apt install parallel'
 	exit 1
 fi
 
 # steghide installation check (steganography tool)
-if [ ! "$(which steghide)" ];
+if [ ! "$(which steghide)" ]
 then
-	echo "ERROR - you need to have steghide installed in order to use the script \"${0}\""
-	echo "INFO - installation in Debian-based distros: sudo apt install steghide"
+	echo 'ERROR - you need to have steghide installed in order to use the script "'"${0}"'"'
+	echo 'INFO - installation in Debian-based distros: sudo apt install steghide'
 	exit 1
 fi
 
 # working directory permissions check
-if [ ! -r "${PWD}" ] || [ ! -w "${PWD}" ];
+if [ ! -r "${PWD}" ] || [ ! -w "${PWD}" ]
 then
-	echo "ERROR - you need read and write permissions in the working directory in order to use the script \"${0}\""
+	echo 'ERROR - you need read and write permissions in the working directory in order to use the script "'"${0}"'"'
 	print_usage
 	exit 1
 fi
 
 # stego file permissions check
-if [ ! -f "${1}" ] || [ ! -r "${1}" ];
+if [ ! -f "${1}" ] || [ ! -r "${1}" ]
 then
-	echo "ERROR - the stego file \"${1}\" passed as 1st argument either does not exist or is not readable"
+	echo 'ERROR - the stego file "'"${1}"'" passed as 1st argument either does not exist or is not readable'
 	print_usage
 	exit 1
 fi
 
 # stego file extension check
-if [[ ! "${1}" =~ ^.*\.(jpg|jpeg|bmp|wav|au)$ ]];
+if [[ ! "${1}" =~ ^.*\.(jpg|jpeg|bmp|wav|au)$ ]]
 then
-	echo "ERROR - the stego file extension used at the 1st argument \"${1}\" is not supported"
-	echo "INFO - supported extensions: jpg, jpeg, bmp, wav, au"
+	echo 'ERROR - the stego file extension used at the 1st argument "'"${1}"'" is not supported'
+	echo 'INFO - supported extensions: jpg, jpeg, bmp, wav, au'
 	print_usage
 	exit 1
 fi
 
 # wordlist file permissions check
-if [ ! -f "${2}" ] || [ ! -r "${2}" ];
+if [ ! -f "${2}" ] || [ ! -r "${2}" ]
 then
-	echo "ERROR - the wordlist file \"${2}\" passed as 2nd argument either does not exist or is not readable"
+	echo 'ERROR - the wordlist file "'"${2}"'" passed as 2nd argument either does not exist or is not readable'
 	print_usage
 	exit 1
 fi
 
 # output file existence check
-if [ -e "${1}.out" ];
+if [ -e "${1}.out" ]
 then
-	echo "ERROR - the output file \"${1}.out\" already exists!"
+	echo 'ERROR - the output file "'"${1}.out"'" already exists!'
 	print_usage
 	exit 1
 fi
 
 # checking finished, stego cracking start
-echo "Trying to crack the stego file \"${1}\" with wordlist \"${2}\" using ${THREADS} threads..."
+echo 'Trying to crack the stego file "'"${1}"'" with wordlist "'"${2}"'" using '"${THREADS}"' threads...'
 
 # dictionary split to distribute the computing load (fragments will be deleted later)
 LINES_PER_THREAD="$(( $(wc --lines "${2}" | awk '{print $1}') / THREADS + 1 ))"
@@ -103,13 +104,13 @@ StegCrusher_function() {
 	do
 
 		# exit if the password has already been found by other thread
-		if [ -f "${2}" ];
+		if [ -f "${2}" ]
 		then
 			exit 0
 
 		# exit if the password is found
 		else
-			if steghide extract --stegofile "${1}" --extractfile "${1}.out" --passphrase "${PASSWORD}" --force --quiet;
+			if steghide 'extract' --stegofile "${1}" --extractfile "${1}.out" --passphrase "${PASSWORD}" --force --quiet
 			then
 				echo -n "${PASSWORD}" > "${2}"
 				exit 0
@@ -125,18 +126,18 @@ parallel --no-notice --arg-sep , StegCrusher_function "${1}" "${WORDLIST_FRAGMEN
 # stego cracking end
 unset -f StegCrusher_function
 
-if [ -f "${1}.out" ];
+if [ -f "${1}.out" ]
 
 # cracking success
 then
-	echo "INFO - crack succeeded, check \"${1}.out\" to see the hidden data in the stego file \"${1}\""
-	echo "INFO - the password used was: $(cat "${WORDLIST_FRAGMENT_NAME}")"
+	echo 'INFO - crack succeeded, check "'"${1}.out"'" to see the hidden data in the stego file "'"${1}"'"'
+	echo 'INFO - the password used was: '"$(cat "${WORDLIST_FRAGMENT_NAME}")"
 	rm --force "${WORDLIST_FRAGMENT_NAME}"*
 	exit 0
 
 # cracking failure
 else
-	echo "ERROR - crack failed, no hidden data found in the stego file \"${1}\" using \"${2}\" as wordlist"
+	echo 'ERROR - crack failed, no hidden data found in the stego file "'"${1}"'" using "'"${2}"'" as wordlist'
 	rm --force "${WORDLIST_FRAGMENT_NAME}"*
 	exit 1
 fi
