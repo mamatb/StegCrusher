182acb2c7d5ecb9a4e33ed8d1d36fd00f0b2290b
mamatb <42239435+mamatb@users.noreply.github.com>
2020-05-16 17:03:57 +0200

    this project has been moved to GitHub

--- StegCrusher.sh
+++ StegCrusher.sh
@@ -2,7 +2,7 @@
 
 # StegCrusher is a steganography brute-force tool that takes advantage of parallel computing
 # author - mamatb (t.me/m_amatb)
-# location - https://gitlab.com/mamatb/StegCrusher
+# location - https://github.com/mamatb/StegCrusher
 
 # acknowledgement - this tool is based on Paradoxis' StegCracker (https://github.com/Paradoxis/StegCracker)
 # acknowledgement - this tool is built upon Stefan Hetzl's Steghide (https://github.com/StefanoDeVuono/steghide)
